0ebe7a55505f0cd00f657a90b1fe35591aa3ad00
Neg00se <evg.matyushko@gmail.com>
2024-08-27 17:19:56 +0300

    Added SignalR chat hub

--- ChatAppWithSentimentAnalyst/Hubs/ChatHub.cs
+++ ChatAppWithSentimentAnalyst/Hubs/ChatHub.cs
@@ -1,10 +1,29 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using ServicesLayer.Interfaces;
 namespace ChatAppWithSentimentAnalyst.Hubs;
 
 public class ChatHub : Hub
 {
+	private readonly IChatService _chatService;
+
+	public ChatHub(IChatService chatService)
+	{
+		_chatService = chatService;
+	}
+
+	public override async Task OnConnectedAsync()
+	{
+		Console.WriteLine("Connected ");
+		var messages = await _chatService.GetAllMessagesAsync();
+		await Clients.Caller.SendAsync("GetMessages", messages);
+	}
+
 	public async Task SendMessage(string user, string message)
 	{
-		await Clients.All.SendAsync("GetMessage", user, message);
+		await _chatService.AddMessage(user, message);
+
+		var messages = await _chatService.GetAllMessagesAsync();
+
+		await Clients.All.SendAsync("GetMessages", messages);
 	}
 }
