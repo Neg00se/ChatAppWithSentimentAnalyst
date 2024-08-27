a90d9ee6034460cdb4c74d14f8e6886636083eba
Neg00se <evg.matyushko@gmail.com>
2024-08-27 17:24:21 +0300

    Added necessary packeges and service dependencies

--- ChatAppWithSentimentAnalyst/Program.cs
+++ ChatAppWithSentimentAnalyst/Program.cs
@@ -1,18 +1,35 @@
 using ChatAppWithSentimentAnalyst.Components;
 using ChatAppWithSentimentAnalyst.Hubs;
+using DataAccessLayer;
+using DataAccessLayer.Interfaces;
+using DataAccessLayer.Repositories;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using ServicesLayer.Interfaces;
+using ServicesLayer.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-	.AddInteractiveServerComponents();
+	.AddInteractiveServerComponents().AddCircuitOptions(options => { options.DetailedErrors = true; });
+
+builder.Services.AddScoped<IChatRepository, ChatRepository>();
+
+builder.Services.AddScoped<IChatService, ChatService>();
+
+builder.Services.AddSingleton<SentimentAnalysisService>();
+
+builder.Services.AddDbContext<ChatAppDbContext>(
+	options => options.UseSqlServer(builder.Configuration.GetConnectionString("azureDb")));
 
 builder.Services.AddResponseCompression(opts =>
 {
 	opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
 		["application/octet-stream"]);
 });
+
+builder.Services.AddSignalR().AddAzureSignalR();
 
 var app = builder.Build();
 
