bad02543350f611c3365f23aad5fbb7fe92fe03f
Neg00se <evg.matyushko@gmail.com>
2024-08-27 17:21:44 +0300

    Added service for sentiment analytics

---
+++ ServicesLayer/Services/SentimentAnalysisService.cs
@@ -0,0 +1,31 @@
+﻿using Azure;
+using Azure.AI.TextAnalytics;
+
+namespace ServicesLayer.Services;
+
+public class SentimentAnalysisService
+{
+	// This example requires environment variables named "LANGUAGE_KEY" and "LANGUAGE_ENDPOINT"
+	static string languageKey = Environment.GetEnvironmentVariable("LANGUAGE_KEY");
+	static string languageEndpoint = Environment.GetEnvironmentVariable("LANGUAGE_ENDPOINT");
+
+	private static readonly AzureKeyCredential credentials = new AzureKeyCredential(languageKey);
+	private static readonly Uri endpoint = new Uri(languageEndpoint);
+	private readonly TextAnalyticsClient _client;
+
+	public SentimentAnalysisService()
+	{
+		_client = new(endpoint, credentials);
+	}
+
+	public async Task<string> Analyze(string message)
+	{
+		Response<DocumentSentiment> response = await _client.AnalyzeSentimentAsync(message);
+		DocumentSentiment docSentiment = response.Value;
+
+		return docSentiment.Sentiment.ToString();
+	}
+
+}
+
+
