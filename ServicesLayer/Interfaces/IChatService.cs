fb190fc3893b8aeaf6e37e007bbc8a8dfe193b23
Neg00se <evg.matyushko@gmail.com>
2024-08-27 17:22:20 +0300

    Added chat service

---
+++ ServicesLayer/Interfaces/IChatService.cs
@@ -0,0 +1,8 @@
+﻿using DataAccessLayer.Entities;
+
+namespace ServicesLayer.Interfaces;
+public interface IChatService
+{
+	Task AddMessage(string user, string message);
+	Task<List<Message>> GetAllMessagesAsync();
+}
---
+++ ServicesLayer/Services/ChatService.cs
@@ -0,0 +1,33 @@
+﻿using DataAccessLayer.Entities;
+using DataAccessLayer.Interfaces;
+using ServicesLayer.Interfaces;
+
+namespace ServicesLayer.Services;
+public class ChatService : IChatService
+{
+	private readonly IChatRepository _chatRepo;
+	private readonly SentimentAnalysisService _analysisService;
+
+	public ChatService(IChatRepository chatRepo, SentimentAnalysisService analysisService)
+	{
+		_chatRepo = chatRepo;
+		_analysisService = analysisService;
+	}
+
+	public async Task<List<Message>> GetAllMessagesAsync()
+	{
+		var messages = await _chatRepo.GetAllAsync();
+
+		return messages;
+	}
+
+	public async Task AddMessage(string user, string message)
+	{
+		Message msg = new Message();
+		msg.User = user;
+		msg.Content = message;
+		msg.Sentiment = await _analysisService.Analyze(message);
+
+		await _chatRepo.CreateAsync(msg);
+	}
+}
